4b44ca9c165dadab7c47c86840fad148a49868af
LiBenjamin <16bnkh@queensu.ca>
2017-06-08 19:50:47 -0400

    safe me

--- test.R
+++ test.R
@@ -1,13 +1,10 @@
 test <- "I hate Chris's dog!"
-<<<<<<< HEAD
 meow <-  2
 fun <-  FALSE
 dog <-TRUE 
-=======
 
 test2 <- "I love Gervais"
 testme <- booyah
 
 a=1
 
->>>>>>> fec7cb8684684c3d6b4292520fe8a8dbc39c12ef
